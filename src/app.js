8f08c4867e8f78cc6266717c6f91f825a26f32c5
Jules Fouchy <jules.fouchy@ntymail.com>
2023-03-20 22:41:13 +0100

    ✨ Adapt to Cyclic

--- src/app.js
+++ src/app.js
@@ -11,64 +11,6 @@ var fs = require("fs")
 var path = require("path")
 require("dotenv/config")
 
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-// Step 5 - set up multer for storing uploaded files
-
-var multer = require("multer")
-
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads")
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + "-" + Date.now())
-  },
-})
-
-var upload = multer({ storage: storage })
-
-// Step 6 - load the mongoose model for Image
-
-var imgModel = require("./model")
-
-// Step 7 - the GET request handler that provides the HTML UI
-
-app.get("/", async (req, res) => {
-  res.send("hello")
-  //   try {
-  //     const articles = await imgModel.find({})
-  //     res.send(articles)
-  //   } catch (err) {
-  //     console.log(err)
-  //     res.status(500).send("An error occurred", err)
-  //   }
-})
-
-// Step 8 - the POST handler for processing the uploaded file
-
-app.post("/", upload.single("image"), async (req, res, next) => {
-  var obj = {
-    name: req.body.name,
-    desc: req.body.desc,
-    img: {
-      data: fs.readFileSync(
-        path.join(__dirname, "../uploads", req.file.filename)
-      ),
-      contentType: "image/png",
-    },
-  }
-  try {
-    const item = await imgModel.create(obj)
-    // item.save();
-    res.redirect("/")
-  } catch {
-    console.log(err)
-  }
-})
-
 // Step 2 - connect to the database
 const connectDB = async () => {
   try {
@@ -86,6 +28,63 @@ const connectDB = async () => {
 var port = process.env.PORT || "3000"
 // Step 3 - code was added to ./models.js
 connectDB().then(() => {
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
+
+  // Step 5 - set up multer for storing uploaded files
+
+  var multer = require("multer")
+
+  var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads")
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + "-" + Date.now())
+    },
+  })
+
+  var upload = multer({ storage: storage })
+
+  // Step 6 - load the mongoose model for Image
+
+  var imgModel = require("./model")
+
+  // Step 7 - the GET request handler that provides the HTML UI
+
+  app.get("/", async (req, res) => {
+    // res.send("hello")
+    try {
+      const articles = await imgModel.find({})
+      res.send(articles)
+    } catch (err) {
+      console.log(err)
+      res.status(500).send("An error occurred", err)
+    }
+  })
+
+  // Step 8 - the POST handler for processing the uploaded file
+
+  app.post("/", upload.single("image"), async (req, res, next) => {
+    var obj = {
+      name: req.body.name,
+      desc: req.body.desc,
+      img: {
+        data: fs.readFileSync(
+          path.join(__dirname, "../uploads", req.file.filename)
+        ),
+        contentType: "image/png",
+      },
+    }
+    try {
+      const item = await imgModel.create(obj)
+      // item.save();
+      res.redirect("/")
+    } catch {
+      console.log(err)
+    }
+  })
+
   app.listen(port, (err) => {
     if (err) throw err
     console.log("Server listening on port", port)
